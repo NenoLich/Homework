bff819974dbf2735bdd791430705c71624e30cc1
Nenolich <ne0fun907@gmail.com>
2018-02-18 19:40:09 +0300

    Removed build from .bat

--- Install.bat
+++ Install.bat
@@ -1,2 +1,1 @@
-msbuild Homework.sln /p:Configuration=Release /p:Platform="Any CPU"
-xcopy /e /h /q "Homework1" "Homework\bin\Debug\Homework1"
+xcopy /e /h /q "Homework1" "Homework\bin\Debug\Homework1\"
